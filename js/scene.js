5d00ea1b8007f6e2de77d8367265aaa86e6acdd0
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-11-11 14:16:31 -0300

    sincronização do projeto

--- js/scene.js
+++ js/scene.js
@@ -109,14 +109,8 @@ function animate(){
 	rotacionarObjeto(terraSistema,EARTH_YEAR);
 	rotacionarObjeto(terraMesh,EARTH_DAY);
 
-	//rotacionarObjeto(marteSistema,EARTH_YEAR/2);
+	rotacionarObjeto(marteSistema,EARTH_YEAR/2);
 	rotacionarObjeto(marteMesh,EARTH_DAY*2);
-
-	
-
-	
-
-	
 
 	rotacionarObjeto(luaTerraSistema,EARTH_DAY);
 
@@ -125,41 +119,3 @@ function animate(){
 }
 
 animate();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.02;
-	cube.rotation.y += 0.02;
-
-	renderer.render( scene, camera );
-};
-
-animate();*/
