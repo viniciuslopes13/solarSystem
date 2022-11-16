879a671735107e1532d3c981e38c2f82d76e7468
viniciuslopes13 <vinicius.lopes.c@hotmail.com>
2022-11-16 20:53:18 -0300

    refactor

--- js/scene.js
+++ js/scene.js
@@ -9,7 +9,6 @@ var fovy = 75,
 const scene = new THREE.Scene();
 scene.background = new THREE.TextureLoader().load("textures/galaxy.jpg");
 
-
 const camera = new THREE.PerspectiveCamera( fovy, aspectRatio, near, far );
 camera.position.set(75, 75, 75);
 camera.lookAt(ORIGEM);
@@ -29,8 +28,6 @@ const terra = new Planet(4, 50, "/textures/earth.jpeg");
 const terraMesh = terra.getMesh();
 const terraSistema = new THREE.Group();
 terraSistema.add(terraMesh);
-const orbitaTerra = new Orbita(terraMesh);
-sistemaSolar.add(orbitaTerra.getMesh());
 
 const luaTerra = new Planet(0.5, 5, "/textures/lua.jpg");
 const luaTerraMesh = luaTerra.getMesh();
@@ -43,36 +40,26 @@ const mercurio = new Planet(0.37*terra.raio, 30, "/textures/mercury.png");
 const mercurioMesh = mercurio.getMesh();
 const mercurioSistema = new THREE.Group();
 mercurioSistema.add(mercurioMesh);
-const orbitaMercurio = new Orbita(mercurioMesh);
-sistemaSolar.add(orbitaMercurio.getMesh());
 
 const venus = new Planet(0.92*terra.raio, 40, "/textures/venus.jpeg");
 const venusMesh = venus.getMesh();
 const venusSistema = new THREE.Group();
 venusSistema.add(venusMesh);
-const orbitaVenus = new Orbita(venusMesh);
-sistemaSolar.add(orbitaVenus.getMesh());
 
 const marte = new Planet(0.51*terra.raio, 60, "/textures/mars.jpeg");
 const marteMesh = marte.getMesh();
 const marteSistema = new THREE.Group();
 marteSistema.add(marteMesh);
-const orbitaMarte = new Orbita(marteMesh);
-sistemaSolar.add(orbitaMarte.getMesh());
 
 const jupiter = new Planet(2*terra.raio, 75, "/textures/jupiter.jpg");
 const jupiterMesh = jupiter.getMesh();
 const jupiterSistema = new THREE.Group();
 jupiterSistema.add(jupiterMesh);
-const orbitaJupiter = new Orbita(jupiterMesh);
-sistemaSolar.add(orbitaJupiter.getMesh());
 
 const saturno = new Planet(2*terra.raio, 95, "/textures/saturno.jpg");
 const saturnoMesh = saturno.getMesh();
 const saturnoSistema = new THREE.Group();
 saturnoSistema.add(saturnoMesh);
-const orbitaSaturno = new Orbita(saturnoMesh);
-sistemaSolar.add(orbitaSaturno.getMesh());
 const geometriaAnelSaturno = new THREE.RingGeometry(saturno.raio+1, saturno.raio+4, 100 );
 const materialAnelSaturno = new THREE.MeshBasicMaterial( { color: 0xF5F6CE, side: THREE.DoubleSide } );
 const anelSaturno = new THREE.Mesh( geometriaAnelSaturno, materialAnelSaturno );
@@ -84,19 +71,25 @@ const urano = new Planet(1.5*terra.raio, 115, "/textures/urano.jpg");
 const uranoMesh = urano.getMesh();
 const uranoSistema = new THREE.Group();
 uranoSistema.add(uranoMesh);
-const orbitaUrano = new Orbita(uranoMesh);
-sistemaSolar.add(orbitaUrano.getMesh());
 
 const neturno = new Planet(1.5*terra.raio, 130, "/textures/neturno.jpg");
 const neturnoMesh = neturno.getMesh();
 const neturnoSistema = new THREE.Group();
 neturnoSistema.add(neturnoMesh);
-const orbitaNeturno = new Orbita(neturnoMesh);
-sistemaSolar.add(orbitaNeturno.getMesh());
-
-
 
 sistemaSolar.add(terraSistema,mercurioSistema,venusSistema,marteSistema,jupiterSistema, saturnoSistema, uranoSistema, neturnoSistema);
+
+var meshs = [terraMesh, mercurioMesh,venusMesh,marteMesh,jupiterMesh,saturnoMesh,uranoMesh,neturnoMesh];
+
+meshs.forEach(element => {
+	addOrbita(element);
+});
+
+
+function addOrbita(planetMesh){
+	const orbita = new Orbita(planetMesh);
+	sistemaSolar.add(orbita.getMesh());
+}
 
 function rotacionarObjeto(objeto, rotacao){
 	var matrizRotacaoEixo = new THREE.Matrix4();
@@ -114,17 +107,18 @@ function rotacionarObjeto(objeto, rotacao){
 }
 
 
-const controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.update();
-
-const EARTH_YEAR = 2 * Math.PI * (1 / 60) * (1 / 60);
-const EARTH_DAY = 2 * Math.PI * (1 / 10) * (1 / 10);
-
 function addLinhaRotacao(planetaMesh, planetaSitema, mostrarLinha = true){
 	const linhaRotacao = new Rotation(planetaMesh,mostrarLinha);
 	const linhaRotacaoMesh = linhaRotacao.getMesh();
 	planetaSitema.add(linhaRotacaoMesh);
 }
+
+
+const controls = new THREE.OrbitControls( camera, renderer.domElement );
+controls.update();
+
+const EARTH_YEAR = 2 * Math.PI * (1 / 60) * (1 / 60);
+const EARTH_DAY = 2 * Math.PI * (1 / 10) * (1 / 10);
 
 addLinhaRotacao(terraMesh,terraSistema);
 
